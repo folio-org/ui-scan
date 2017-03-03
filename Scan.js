6020a49aac5abea66db8af3492771d66f37aab35
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-03-03 21:57:18 +0100

    Persists checkouts and sets UI state flag for pending scan operation.
      The flag intends to control whether the state should be retained
      when user navigates away from Scan. State is retained if there
      are scanned items that have yet to be checked out, otherwise the
      state is cleared.

--- Scan.js
+++ Scan.js
@@ -20,6 +20,7 @@ class Scan extends React.Component{
     mode: {},
     patrons: {},
     items: {},
+    pendingScan: {},
     itemsx: {
       type: 'okapi',
       path: 'item-storage/items/:{itemid}',
@@ -49,16 +50,26 @@ class Scan extends React.Component{
   }
 
   componentWillMount() {
-    const { data: { items, mode, patrons }, mutator } = this.props;
+    const { data: { items, mode, patrons, pendingScan }, mutator } = this.props;
+
+    if (_.isEmpty(pendingScan)) {
+      mutator.pendingScan.replace({state: false});
+    }
 
     if (_.isEmpty(mode)) {
       mutator.mode.replace('CheckOut');
     }
-    if (_.isEmpty(items)) {
+
+    if (!pendingScan || !pendingScan.state) {
       mutator.items.replace([]);
-    }
-    if (_.isEmpty(patrons)) {
       mutator.patrons.replace([]);
+    } else {
+      if (_.isEmpty(items)) {
+        mutator.items.replace([]);
+      }
+      if (_.isEmpty(patrons)) {
+        mutator.patrons.replace([]);
+      }
     }
   }
 
@@ -69,8 +80,12 @@ class Scan extends React.Component{
 
   onClickFindPatron(e) {
     if (e) e.preventDefault();
+    this.props.mutator.items.replace([]);
     let username = document.getElementById('patronid').value;
-    fetch(`${this.okapiUrl}/users?query=(username="${username}")`, { headers: Object.assign({}, { 'X-Okapi-Tenant': this.tenant, 'X-Okapi-Token': this.store.getState().okapi.token }) })
+    fetch(`${this.okapiUrl}/users?query=(username="${username}")`,
+          { headers: Object.assign({}, { 'X-Okapi-Tenant': this.tenant,
+                                         'X-Okapi-Token': this.store.getState().okapi.token,
+                                         'Content-Type': 'application/json' }) })
     .then((response) => {
       if (response.status >= 400) {
         console.log("Error fetching user");
@@ -84,19 +99,26 @@ class Scan extends React.Component{
 
   onClickAddItem(e) {
     if (e) e.preventDefault();
+    this.props.mutator.pendingScan.replace({state: true});
     const barcodeValue = document.getElementById('barcode').value;
     const barcodes = barcodeValue.split(" ");
     for (var i = 0; i < barcodes.length; i++) {
       let barcode = barcodes[i].trim();
       if (barcode) {
-        fetch(`${this.okapiUrl}/item-storage/items?query=(barcode="${barcode}")`, { headers: Object.assign({}, { 'X-Okapi-Tenant': this.tenant, 'X-Okapi-Token': this.store.getState().okapi.token }) })
+        fetch(`${this.okapiUrl}/item-storage/items?query=(barcode="${barcode}")`,
+               { headers: Object.assign({}, {'X-Okapi-Tenant': this.tenant,
+                                             'X-Okapi-Token': this.store.getState().okapi.token,
+                                             'Content-Type': 'application/json' }) })
         .then((response) => {
           if (response.status >= 400) {
             console.log("Error fetching user");
           } else {
             response.json().then((json) => {
-              let items = [].concat(this.props.data.items).concat(json.items);
-              this.props.mutator.items.replace(items);
+              let itemid = json.items[0].id;
+              if (this.props.data.items.findIndex((item)=>{ return (item.id === itemid);})<0) {
+                let items = [].concat(this.props.data.items).concat(json.items);
+                this.props.mutator.items.replace(items);
+              }
             });
           }
         });
@@ -110,15 +132,35 @@ class Scan extends React.Component{
     const index = items.findIndex((item) => { return (item.id === itemid); })
     items.splice(index,1);
     this.props.mutator.items.replace(items);
+    if (items.length==0) this.props.mutator.pendingScan.replace({state: false});
   }
 
   onClickCheckout () {
     let items = JSON.parse(JSON.stringify(this.props.data.items));
     for (var i = 0; i < items.length ; i++) {
       items[i].status.name = 'Checked Out';
+      if (this.putItem(items[i])) {
+        this.props.mutator.items.replace(items);
+      }
     }
-    this.props.mutator.items.replace(items);
+    this.props.mutator.pendingScan.replace({state: false});
   }
+
+  putItem(item) {
+    fetch(`${this.okapiUrl}/item-storage/items/${item.id}`, {
+      method: 'PUT',
+      headers: Object.assign({}, { 'X-Okapi-Tenant': this.tenant, 'X-Okapi-Token': this.store.getState().okapi.token, 'Content-Type': 'application/json' }),
+      body: JSON.stringify(item),
+    }).then((response) => {
+      if (response.status >= 400) {
+        console.log(`Users. POST of item ${item.barcode} failed.`);
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+
 
   render() {
     const { data: { mode, items, patrons } } = this.props;
