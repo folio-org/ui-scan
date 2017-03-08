3ad7869f4b8a601af47348184298ba29fe728f18
Mike Taylor <mike@indexdata.com>
2017-03-08 16:51:26 +0000

    Convert to a functional-style React component. Fixes last proper ESLint error. (But an inexplicable and obviously incorrect react/no-unused-prop-types remains)

--- CheckIn.js
+++ CheckIn.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+// We have to remove node_modules/react to avoid having multiple copies loaded.
+// eslint-disable-next-line import/no-unresolved
 import React from 'react';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import Pane from '@folio/stripes-components/lib/Pane';
@@ -15,52 +17,50 @@ const propTypes = {
   onClickCheckin: React.PropTypes.func,
 };
 
-class CheckIn extends React.Component {
-  render() {
-    const containerStyle = {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      height: '100%',
-      width: '100%',
-      position: 'absolute',
-    };
+function CheckIn(props) {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+  };
 
-    const itemListFormatter = {
-      status: item => `${_.get(item, ['status', 'name'], '')}`,
-      '': item => <td key={item.id}><Button buttonStyle="negative hollow" align="end" marginBottom0 onClick={() => { this.props.onClickRemoveItem(item.id); }}>Cancel</Button></td>,
-    };
+  const itemListFormatter = {
+    status: item => `${_.get(item, ['status', 'name'], '')}`,
+    '': item => <td key={item.id}><Button buttonStyle="negative hollow" align="end" marginBottom0 onClick={() => { props.onClickRemoveItem(item.id); }}>Cancel</Button></td>,
+  };
 
-    return (
-      <div style={containerStyle}>
-        <Paneset static>
-          <Pane paneTitle="Scanned Items" defaultWidth="100%" firstMenu={this.props.modeSelector}>
-            <div style={{ width: '100%', maxWidth: '1024px', margin: 'auto' }}>
-              <Row>
-                <Col xs={9}>
-                  <TextField placeholder="Enter Barcode" aria-label="Item ID" fullWidth id="barcode" />
-                </Col>
-                <Col xs={3}>
-                  <Button buttonStyle="primary noRadius" fullWidth onClick={this.props.onClickAddItem}>+ Add item</Button>
-                </Col>
-              </Row>
-              <MultiColumnList
-                visibleColumns={['barcode', 'status', '']}
-                rowMetadata={['id']}
-                contentData={this.props.items}
-                formatter={itemListFormatter}
-                isEmptyMessage="No items have been entered yet."
-                fullWidth
-              />
-            </div>
-          </Pane>
-        </Paneset>
-        {this.props.items && this.props.items.length !== 0 &&
-          <Button buttonStyle="primary mega" onClick={this.props.onClickCheckin}>Done</Button>
-        }
-      </div>
-    );
-  }
+  return (
+    <div style={containerStyle}>
+      <Paneset static>
+        <Pane paneTitle="Scanned Items" defaultWidth="100%" firstMenu={props.modeSelector}>
+          <div style={{ width: '100%', maxWidth: '1024px', margin: 'auto' }}>
+            <Row>
+              <Col xs={9}>
+                <TextField placeholder="Enter Barcode" aria-label="Item ID" fullWidth id="barcode" />
+              </Col>
+              <Col xs={3}>
+                <Button buttonStyle="primary noRadius" fullWidth onClick={props.onClickAddItem}>+ Add item</Button>
+              </Col>
+            </Row>
+            <MultiColumnList
+              visibleColumns={['barcode', 'status', '']}
+              rowMetadata={['id']}
+              contentData={props.items}
+              formatter={itemListFormatter}
+              isEmptyMessage="No items have been entered yet."
+              fullWidth
+            />
+          </div>
+        </Pane>
+      </Paneset>
+      {props.items && props.items.length !== 0 &&
+        <Button buttonStyle="primary mega" onClick={props.onClickCheckin}>Done</Button>
+      }
+    </div>
+  );
 }
 
 CheckIn.propTypes = propTypes;
