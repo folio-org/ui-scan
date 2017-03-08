8a609735c9aba0d69a8e07b22d6fc53e8ae23894
Mike Taylor <mike@indexdata.com>
2017-03-08 17:30:20 +0000

    Fix all ESLint errors except pure-finction.

--- UserView.js
+++ UserView.js
@@ -1,61 +1,56 @@
+import _ from 'lodash';
+// We have to remove node_modules/react to avoid having multiple copies loaded.
+// eslint-disable-next-line import/no-unresolved
 import React from 'react';
 
-import Paneset from '@folio/stripes-components/lib/Paneset';
-import Pane from '@folio/stripes-components/lib/Pane';
-import KeyValue from '@folio/stripes-components/lib/KeyValue'
-import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
-import {Row, Col} from 'react-bootstrap';
+import KeyValue from '@folio/stripes-components/lib/KeyValue';
+import { Row, Col } from 'react-bootstrap';
 
 const propTypes = {
   user: React.PropTypes.object,
-}
+};
 
-export default class UserView extends React.Component{
-
-  constructor(props) {
-    super(props);
-  }
-
+export default class UserView extends React.Component {
   render() {
     const user = this.props.user;
     const userStatus = (_.get(user, ['active'], '') ? 'active' : 'inactive');
 
     return (
-        <Row>
-          <Col xs={8} >
-            <Row>
-              <Col xs={12}>
-                <h2>{_.get(user, ['personal', 'last_name'], '')}, {_.get(user, ['personal', 'first_name'], '')}</h2>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={12}>
-                <KeyValue label="Username" value={_.get(user, ['username'], '')} />
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col xs={12}>
-                <KeyValue label="Status" value={userStatus} />
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col xs={12}>
-                <KeyValue label="Email" value={_.get(user, ['personal', 'email'], '')} />
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col xs={12}>
-                <KeyValue label="Patron group" value={_.get(user, ['patron_group'], '')} />
-              </Col>
-            </Row>
-          </Col>
-          <Col xs={4} >
-            <img className="floatEnd" src="http://placehold.it/175x175" role="presentation" />
-          </Col>
-        </Row>);
+      <Row>
+        <Col xs={8} >
+          <Row>
+            <Col xs={12}>
+              <h2>{_.get(user, ['personal', 'last_name'], '')}, {_.get(user, ['personal', 'first_name'], '')}</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <KeyValue label="Username" value={_.get(user, ['username'], '')} />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs={12}>
+              <KeyValue label="Status" value={userStatus} />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs={12}>
+              <KeyValue label="Email" value={_.get(user, ['personal', 'email'], '')} />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs={12}>
+              <KeyValue label="Patron group" value={_.get(user, ['patron_group'], '')} />
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={4} >
+          <img className="floatEnd" src="http://placehold.it/175x175" role="presentation" />
+        </Col>
+      </Row>);
   }
 }
 
