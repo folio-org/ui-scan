a0ee32b52e8af38ef0f4dd7ed0d22d2415058dee
Mike Taylor <mike@indexdata.com>
2017-03-08 13:43:05 +0000

    Fix many simple ELint errors.

--- CheckIn.js
+++ CheckIn.js
@@ -1,30 +1,25 @@
+import _ from 'lodash';
 import React from 'react';
-
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import Pane from '@folio/stripes-components/lib/Pane';
-import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
-import Select from '@folio/stripes-components/lib/Select';
 import Button from '@folio/stripes-components/lib/Button';
 import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList';
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import TextField from '@folio/stripes-components/lib/TextField';
 
 const propTypes = {
-  onChangeMode: React.PropTypes.func,
   modeSelector: React.PropTypes.element,
   items: React.PropTypes.arrayOf(React.PropTypes.object),
-  patron: React.PropTypes.object,   // eslint-disable-line
-}
+  onClickAddItem: React.PropTypes.func,
+  onClickRemoveItem: React.PropTypes.func,
+  onClickCheckin: React.PropTypes.func,
+};
 
-class CheckIn extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  
-  render(){
+class CheckIn extends React.Component {
+  render() {
     const containerStyle = {
       display: 'flex',
-      flexDirection:'column',
+      flexDirection: 'column',
       justifyContent: 'space-between',
       height: '100%',
       width: '100%',
@@ -33,40 +28,41 @@ class CheckIn extends React.Component{
 
     const itemListFormatter = {
       status: item => `${_.get(item, ['status', 'name'], '')}`,
-      '': item => <td key={item.id}><Button buttonStyle="negative hollow" align="end" marginBottom0 onClick={()=> {this.props.onClickRemoveItem(item.id);}}>Cancel</Button></td>,
+      '': item => <td key={item.id}><Button buttonStyle="negative hollow" align="end" marginBottom0 onClick={() => { this.props.onClickRemoveItem(item.id); }}>Cancel</Button></td>,
     };
 
-    return(
+    return (
       <div style={containerStyle}>
-      <Paneset static>
-        <Pane paneTitle="Scanned Items" defaultWidth='100%' firstMenu={this.props.modeSelector}>
-          <div style={{width:'100%', maxWidth:'1024px', margin:'auto'}}>
-            <Row>
-              <Col xs={9}>
-                <TextField placeholder='Enter Barcode' aria-label='Item ID' fullWidth id="barcode"/>
-              </Col>
-              <Col xs={3}>
-              <Button buttonStyle="primary noRadius" fullWidth onClick={this.props.onClickAddItem}>+ Add item</Button>
-              </Col>
-            </Row>
-            <MultiColumnList 
-              visibleColumns={['barcode', 'status', '']}
-              rowMetadata={['id']}
-              contentData={this.props.items} 
-              formatter={itemListFormatter} 
-              isEmptyMessage="No items have been entered yet."
-              fullWidth
-            />
-          </div>
-        </Pane>
-      </Paneset>
-      {this.props.items && this.props.items.length !== 0 &&
+        <Paneset static>
+          <Pane paneTitle="Scanned Items" defaultWidth="100%" firstMenu={this.props.modeSelector}>
+            <div style={{ width: '100%', maxWidth: '1024px', margin: 'auto' }}>
+              <Row>
+                <Col xs={9}>
+                  <TextField placeholder="Enter Barcode" aria-label="Item ID" fullWidth id="barcode" />
+                </Col>
+                <Col xs={3}>
+                  <Button buttonStyle="primary noRadius" fullWidth onClick={this.props.onClickAddItem}>+ Add item</Button>
+                </Col>
+              </Row>
+              <MultiColumnList
+                visibleColumns={['barcode', 'status', '']}
+                rowMetadata={['id']}
+                contentData={this.props.items}
+                formatter={itemListFormatter}
+                isEmptyMessage="No items have been entered yet."
+                fullWidth
+              />
+            </div>
+          </Pane>
+        </Paneset>
+        {this.props.items && this.props.items.length !== 0 &&
           <Button buttonStyle="primary mega" onClick={this.props.onClickCheckin}>Done</Button>
         }
       </div>
-    )
+    );
   }
-  
 }
+
+CheckIn.propTypes = propTypes;
 
 export default CheckIn;
