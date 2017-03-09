ab4e5c5c70f2cf8e7a343eeeaec70ccf88e14070
Mike Taylor <mike@indexdata.com>
2017-03-09 11:42:23 +0000

    Fix all ESLint errors except arrow-body-style, no-undef and react/prop-types -- these will need more thought.

--- Scan.js
+++ Scan.js
@@ -1,22 +1,23 @@
 import _ from 'lodash';
+// We have to remove node_modules/react to avoid having multiple copies loaded.
+// eslint-disable-next-line import/no-unresolved
 import React, { PropTypes } from 'react';
-
+import fetch from 'isomorphic-fetch';
 import { connect } from '@folio/stripes-connect';
-
 import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
 import Select from '@folio/stripes-components/lib/Select';
 
-//import Automatic from './Automatic';
+// import Automatic from './Automatic';
 import CheckIn from './CheckIn';
 import CheckOut from './CheckOut';
 
-class Scan extends React.Component{
+class Scan extends React.Component {
 
   static contextTypes = {
     store: PropTypes.object,
   }
 
-  static manifest = Object.freeze ({
+  static manifest = Object.freeze({
     mode: {},
     patrons: {},
     items: {},
@@ -28,7 +29,7 @@ class Scan extends React.Component{
     },
   });
 
-  constructor(props, context){
+  constructor(props, context) {
     super(props);
 
     this.store = context.store;
@@ -37,11 +38,11 @@ class Scan extends React.Component{
     this.tenant = this.sys.okapi.tenant;
 
     this.componentMap = {
-      'CheckOut': CheckOut,
-      'CheckIn': CheckIn,
-      //'Automatic': Automatic,
+      CheckOut,
+      CheckIn,
+      // Automatic: Automatic,
     };
-    
+
     this.onChangeMode = this.onChangeMode.bind(this);
     this.onClickFindPatron = this.onClickFindPatron.bind(this);
     this.onClickAddItem = this.onClickAddItem.bind(this);
@@ -54,7 +55,7 @@ class Scan extends React.Component{
     const { data: { items, mode, patrons, pendingScan }, mutator } = this.props;
 
     if (_.isEmpty(pendingScan)) {
-      mutator.pendingScan.replace({state: false});
+      mutator.pendingScan.replace({ state: false });
     }
 
     if (_.isEmpty(mode)) {
@@ -74,10 +75,10 @@ class Scan extends React.Component{
     }
   }
 
-  onChangeMode(e){
+  onChangeMode(e) {
     const nextMode = e.target.value;
     this.props.mutator.mode.replace(nextMode);
-    this.props.mutator.pendingScan.replace({state: false});
+    this.props.mutator.pendingScan.replace({ state: false });
     this.props.mutator.items.replace([]);
     this.props.mutator.patrons.replace([]);
   }
@@ -85,14 +86,16 @@ class Scan extends React.Component{
   onClickFindPatron(e) {
     if (e) e.preventDefault();
     this.props.mutator.items.replace([]);
-    let username = document.getElementById('patronid').value;
+    const username = document.getElementById('patronid').value;
     fetch(`${this.okapiUrl}/users?query=(username="${username}")`,
-          { headers: Object.assign({}, { 'X-Okapi-Tenant': this.tenant,
-                                         'X-Okapi-Token': this.store.getState().okapi.token,
-                                         'Content-Type': 'application/json' }) })
+      { headers: Object.assign({}, {
+        'X-Okapi-Tenant': this.tenant,
+        'X-Okapi-Token': this.store.getState().okapi.token,
+        'Content-Type': 'application/json' }),
+      })
     .then((response) => {
       if (response.status >= 400) {
-        console.log("Error fetching user");
+        console.log('Error fetching user');
       } else {
         response.json().then((json) => {
           this.props.mutator.patrons.replace(json.users);
@@ -103,24 +106,26 @@ class Scan extends React.Component{
 
   onClickAddItem(e) {
     if (e) e.preventDefault();
-    this.props.mutator.pendingScan.replace({state: true});
+    this.props.mutator.pendingScan.replace({ state: true });
     const barcodeValue = document.getElementById('barcode').value;
-    const barcodes = barcodeValue.split(" ");
-    for (var i = 0; i < barcodes.length; i++) {
-      let barcode = barcodes[i].trim();
+    const barcodes = barcodeValue.split(' ');
+    for (let i = 0; i < barcodes.length; i++) {
+      const barcode = barcodes[i].trim();
       if (barcode) {
         fetch(`${this.okapiUrl}/item-storage/items?query=(barcode="${barcode}")`,
-               { headers: Object.assign({}, {'X-Okapi-Tenant': this.tenant,
-                                             'X-Okapi-Token': this.store.getState().okapi.token,
-                                             'Content-Type': 'application/json' }) })
+          { headers: Object.assign({}, {
+            'X-Okapi-Tenant': this.tenant,
+            'X-Okapi-Token': this.store.getState().okapi.token,
+            'Content-Type': 'application/json',
+          }) })
         .then((response) => {
           if (response.status >= 400) {
-            console.log("Error fetching user");
+            console.log('Error fetching user');
           } else {
             response.json().then((json) => {
-              let itemid = json.items[0].id;
-              if (this.props.data.items.findIndex((item)=>{ return (item.id === itemid);})<0) {
-                let items = [].concat(this.props.data.items).concat(json.items);
+              const itemid = json.items[0].id;
+              if (this.props.data.items.findIndex((item) => { return (item.id === itemid); }) < 0) {
+                const items = [].concat(this.props.data.items).concat(json.items);
                 this.props.mutator.items.replace(items);
               }
             });
@@ -132,29 +137,29 @@ class Scan extends React.Component{
   }
 
   onClickRemoveItem(itemid) {
-    let items = JSON.parse(JSON.stringify(this.props.data.items));
-    const index = items.findIndex((item) => { return (item.id === itemid); })
-    items.splice(index,1);
+    const items = JSON.parse(JSON.stringify(this.props.data.items));
+    const index = items.findIndex((item) => { return (item.id === itemid); });
+    items.splice(index, 1);
     this.props.mutator.items.replace(items);
-    if (items.length==0) this.props.mutator.pendingScan.replace({state: false});
+    if (items.length === 0) this.props.mutator.pendingScan.replace({ state: false });
   }
 
   onClickCheckout() {
-    let items = JSON.parse(JSON.stringify(this.props.data.items));
-    for (var i = 0; i < items.length ; i++) {
+    const items = JSON.parse(JSON.stringify(this.props.data.items));
+    for (let i = 0; i < items.length; i++) {
       items[i].status.name = 'Checked Out';
-      this.putItem(items,i);
+      this.putItem(items, i);
     }
-    this.props.mutator.pendingScan.replace({state: false});
+    this.props.mutator.pendingScan.replace({ state: false });
   }
 
   onClickCheckin() {
-    let items = JSON.parse(JSON.stringify(this.props.data.items));
-    for (var i = 0; i < items.length ; i++) {
+    const items = JSON.parse(JSON.stringify(this.props.data.items));
+    for (let i = 0; i < items.length; i++) {
       items[i].status.name = 'Available';
-      this.putItem(items,i);
+      this.putItem(items, i);
     }
-    this.props.mutator.pendingScan.replace({state: false});
+    this.props.mutator.pendingScan.replace({ state: false });
   }
 
   putItem(items, i) {
@@ -167,8 +172,8 @@ class Scan extends React.Component{
         throw Error(response.statusText);
       }
       return response;
-    }).then((response) => {
-     this.props.mutator.items.replace(items);
+    }).then(() => {
+      this.props.mutator.items.replace(items);
     }).catch((error) => {
       console.log(error);
     });
@@ -176,29 +181,29 @@ class Scan extends React.Component{
 
   render() {
     const { data: { mode, items, patrons } } = this.props;
-    if (!mode) return <div/>;
+    if (!mode) return <div />;
     const modeOptions = [
       { label: 'Check items out', value: 'CheckOut' },
       /* { label: 'Automatic Mode', value: 'Automatic' }, */
-      { label: 'Check items in', value: 'CheckIn' }
+      { label: 'Check items in', value: 'CheckIn' },
     ];
-    
+
     const modeMenu = (
-      <PaneMenu><Select marginBottom0 dataOptions={modeOptions} value={mode} onChange={this.onChangeMode}></Select></PaneMenu>
+      <PaneMenu><Select marginBottom0 dataOptions={modeOptions} value={mode} onChange={this.onChangeMode} /></PaneMenu>
     );
-    
-    return React.createElement(this.componentMap[mode],
-                               {onChangeMode: this.onChangeMode,
-                                modeSelector: modeMenu,
-                                onClickFindPatron: this.onClickFindPatron,
-                                onClickAddItem: this.onClickAddItem,
-                                onClickRemoveItem: this.onClickRemoveItem,
-                                onClickCheckout: this.onClickCheckout,
-                                onClickCheckin: this.onClickCheckin,
-                                patrons,
-                                items});
+
+    return React.createElement(this.componentMap[mode], {
+      onChangeMode: this.onChangeMode,
+      modeSelector: modeMenu,
+      onClickFindPatron: this.onClickFindPatron,
+      onClickAddItem: this.onClickAddItem,
+      onClickRemoveItem: this.onClickRemoveItem,
+      onClickCheckout: this.onClickCheckout,
+      onClickCheckin: this.onClickCheckin,
+      patrons,
+      items,
+    });
   }
-  
 }
 
-export default connect(Scan,'@folio/scan');
+export default connect(Scan, '@folio/scan');
