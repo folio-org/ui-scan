085f5c93c86eb1feae3c8b0980669761a51f64ee
Baroquem <mjc12@cornell.edu>
2017-04-30 17:22:40 -0400

    Fix linting errors

--- settings/ScanCheckoutSettings.js
+++ settings/ScanCheckoutSettings.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { PropTypes } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { connect } from '@folio/stripes-connect';
-import Paneset from '@folio/stripes-components/lib/Paneset';
+import { connect } from '@folio/stripes-connect'; // eslint-disable-line
 import Pane from '@folio/stripes-components/lib/Pane';
 import Select from '@folio/stripes-components/lib/Select';
-import makePathFunction from '@folio/stripes-components/util/makePathFunction';
-
 
 class ScanCheckoutSettings extends React.Component {
+
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    mutator: PropTypes.shape({
+      userIdentifierPrefRecordId: PropTypes.shape({
+        replace: PropTypes.func,
+      }),
+      userIdentifierPref: PropTypes.shape({
+        POST: PropTypes.func,
+        PUT: PropTypes.func,
+      }),
+    }).isRequired,
+    paneWidth: PropTypes.string.isRequired,
+  };
 
   static manifest = Object.freeze({
     userIdentifierPrefRecordId: {},
@@ -21,68 +31,68 @@ class ScanCheckoutSettings extends React.Component {
       },
       PUT: {
         path: 'configurations/entries/${userIdentifierPrefRecordId}',
-      }
-    }
+      },
+    },
   });
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      //selectedIdentifier: {}
-    }
+      // selectedIdentifier: {}
+    };
 
     this.onChangeIdentifier = this.onChangeIdentifier.bind(this);
-
   }
 
   onChangeIdentifier(e) {
-    console.log("ID type selected", e.target.value);
-    let prefRecord = this.props.data.userIdentifierPref[0];
+    console.log('ID type selected', e.target.value);
+    const prefRecord = this.props.data.userIdentifierPref[0];
     if (prefRecord) {
       // preference has been set previously, can proceed with update here
       this.props.mutator.userIdentifierPrefRecordId.replace(prefRecord.id);
       prefRecord.value = e.target.value;
       this.props.mutator.userIdentifierPref.PUT(prefRecord);
-    }
-    else {
+    } else {
       // no preference exists, so create a new one
       this.props.mutator.userIdentifierPref.POST(
         {
-          "module": "SCAN",
-          "config_name": "pref_patron_identifier",
-          "value": e.target.value,
-        }
+          module: 'SCAN',
+          config_name: 'pref_patron_identifier',
+          value: e.target.value,
+        },
       );
     }
   }
 
   render() {
     let selectedIdentifier = this.props.data.userIdentifierPref || [];
-    if (selectedIdentifier.length > 0)
+    if (selectedIdentifier.length > 0) {
       selectedIdentifier = selectedIdentifier[0].value;
+    }
 
     const identifierTypes = [
       { key: 'BARCODE', label: 'Barcode' },
       { key: 'EXTERNAL', label: 'External System ID' },
       { key: 'FOLIO', label: 'FOLIO Record Number' },
-      { key: 'USER', label: 'User ID' }
+      { key: 'USER', label: 'User ID' },
     ];
 
-    const identifierTypeOptions = identifierTypes.map(i => {
-      return {
+    const identifierTypeOptions = identifierTypes.map(i => (
+      {
         label: i.label,
-        value: i.key
-      };
-    });
+        value: i.key,
+      }
+    ));
 
     return (
       <Pane defaultWidth={this.props.paneWidth} paneTitle="Check-out">
         <Row>
           <Col xs={12}>
-            <label>Scan ID for patron check-out</label>
-            <br/>
+            <label htmlFor="patronScanId">Scan ID for patron check-out</label>
+            <br />
             <Select
+              id="patronScanId"
               placeholder="---"
               value={selectedIdentifier}
               dataOptions={identifierTypeOptions}
