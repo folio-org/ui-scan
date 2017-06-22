90a2e102e0327c901c9617563c71241ed37daa12
Michal Kuklis <michal.kuklis@gmail.com>
2017-06-22 10:28:08 -0400

    Add externalSystemId to external queryKey

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const patronIdentifierTypes = [
   { key: 'BARCODE', label: 'Barcode', queryKey: 'barcode' },
-  { key: 'EXTERNAL', label: 'External System ID', queryKey: '' },
+  { key: 'EXTERNAL', label: 'External System ID', queryKey: 'externalSystemId' },
   { key: 'FOLIO', label: 'FOLIO Record Number', queryKey: 'id' },
   { key: 'USER', label: 'User ID', queryKey: 'username' },
 ];
