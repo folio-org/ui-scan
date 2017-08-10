030b7497e6b88900fcb3fe36de69d910c3276fce
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-10 13:37:24 +0200

    Rename 'User ID' 'Username' in list passed to MaybeUserSearch. UIS-81.

--- CheckOut.js
+++ CheckOut.js
@@ -165,7 +165,7 @@ class CheckOut extends React.Component {
                     fullWidth
                     id="patron_identifier"
                     component={TextField}
-                    startControl={<MaybeUserSearch {...this.props.parentProps} selectUser={this.autoSelectUser} visibleColumns={['Name', 'Patron Group', 'User ID', 'Barcode']} dissableUserCreation={true} />}
+                    startControl={<MaybeUserSearch {...this.props.parentProps} selectUser={this.autoSelectUser} visibleColumns={['Name', 'Patron Group', 'Username', 'Barcode']} dissableUserCreation={true} />}
                     onKeyDown={e => this.handleAdd(e, 'find_patron')}
                   />
                 </Col>
